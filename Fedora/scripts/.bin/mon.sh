0fec9d1205ae54b9dc4cb9e44afee5646fb6e70a
Syakhisk <mail.syakhisk@gmail.com>
2022-07-23 15:20:30 +0700

    Update

--- Fedora/scripts/.bin/mon.sh
+++ Fedora/scripts/.bin/mon.sh
@@ -2,8 +2,12 @@
 
 # get percentage of ram and cpu usage
 
-MEM="$(free -t | awk 'FNR == 2 {printf("%.2f%"), $3/$2*100}')"
-USG="$(mpstat 1 1 | awk 'END{print 100-$NF"%"}')"
+# MEM="$(free -t | awk 'FNR == 2 {printf("%.2f%"), $3/$2*100}')"
+# USG="$(mpstat 1 1 | awk 'END{printf 100-$NF"%"}')"
+# STO="$(df -h | awk 'FNR==4 {printf "%s/%s", $3,$2}')"
+
+MEM="$(free -t | awk 'FNR == 2 {printf "%05.2f", $3/$2*100}')"
+USG="$(mpstat 1 1 | awk 'END{printf "%05.2f%", 100-$NF}')"
 STO="$(df -h | awk 'FNR==4 {printf "%s/%s", $3,$2}')"
 
 echo "M: $MEM | C: $USG | S: $STO"
