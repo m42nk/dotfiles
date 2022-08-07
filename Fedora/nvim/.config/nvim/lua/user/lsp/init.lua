ec434d662e6f08b290cf1a3f14ff2bbdf9264b52
Syakhisk Al-azmi <mail.syakhisk@gmail.com>
2022-08-07 16:03:50 +0700

    Move nvim config to submodule

--- Fedora/nvim/.config/nvim/lua/user/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local ok, _ = pcall(require, "lspconfig")
-if not ok then
-  return
-end
-
-require "user.lsp.mason"
-require "user.lsp.null-ls"
-require "user.lsp.ui"
-require "user.lsp.fidget"
--- Fedora/nvim/.config/nvim/plugin/iswap.lua
+++
@@ -1,6 +0,0 @@
-local ok, iswap = pcall(require, "iswap")
-if not ok then
-  return
-end
-
-iswap.setup {}
