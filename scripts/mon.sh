e614d8bbbc93f02e6610e2673ede3e065569f542
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-01-04 00:09:45 +0700

    add generals

---
+++ scripts/mon.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+
+# get percentage of ram and cpu usage
+
+# MEM="$(free -t | awk 'FNR == 2 {printf("%.2f%"), $3/$2*100}')"
+# USG="$(mpstat 1 1 | awk 'END{printf 100-$NF"%"}')"
+# STO="$(df -h | awk 'FNR==4 {printf "%s/%s", $3,$2}')"
+
+STO_MOUNTED_ON="/"
+
+MEM="$(free -t | awk 'FNR == 2 {printf "%05.2f", $3/$2*100}')"
+USG="$(mpstat 1 1 | awk 'END{printf "%05.2f%", 100-$NF}')"
+STO="$(df -h -x tmpfs | grep "${STO_MOUNTED_ON}$" | awk 'FNR==1 {printf "%s/%s", $3,$2}')"
+
+echo "M: $MEM | C: $USG | S: $STO"
