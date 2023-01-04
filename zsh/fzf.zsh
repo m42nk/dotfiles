de960f40b8dc78104b5c369a784e09eb388b26a6
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-01-04 09:41:52 +0700

    zsh fzf bindings

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -3,8 +3,6 @@ BASE=$(dirname "$0")
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --exclude node_modules'
 export FZF_DEFAULT_OPTS="--bind tab:down,btab:up --layout reverse --cycle"
 export FZF_ALT_C_COMMAND='fd --type d --follow --exclude node_modules'
-export FZF_COMPLETION_TRIGGER='@'
-
 if [[ -f ~/.fzf.zsh ]]; then
   source ~/.fzf.zsh
 fi
@@ -12,6 +10,9 @@ fi
 if [[ -f "${BASE}/etc_profile.d_fzf.zsh" ]]; then
   source ${BASE}/etc_profile.d_fzf.zsh
 fi
+
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+source /usr/share/doc/fzf/examples/completion.zsh
 
 _fzf_compgen_path() {
   fd --hidden --follow --exclude ".git" . "$1"
