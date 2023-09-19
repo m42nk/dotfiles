a0b78e8545d78d647f55e56705fd9b70bfc68abb
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2023-09-20 05:34:18 +0700

    update

--- packages/zsh/asdf.zsh
+++ packages/zsh/asdf.zsh
@@ -2,6 +2,9 @@ export ASDF_KERL_VERSION="2.2.4"
 
 if [[ -f "$HOME/.asdf/asdf.sh" ]]; then
   source $HOME/.asdf/asdf.sh
+
+  [[ -d $HOME/.asdf/plugins/golang ]] && source $HOME/.asdf/plugins/golang/set-env.zsh
+
   fpath=(${ASDF_DIR}/completions $fpath)
 elif [[ $(command -v brew) ]] && [[ $(command -v asdf) ]]; then
   source $(brew --prefix)/opt/asdf/libexec/asdf.sh
