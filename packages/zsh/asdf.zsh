d68878c03910f561f04c9ae4944073c88cee55b7
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2023-11-25 22:23:53 +0700

    update

--- packages/zsh/asdf.zsh
+++ packages/zsh/asdf.zsh
@@ -5,6 +5,9 @@ if [[ $(command -v asdf) ]] || [[ -f "$HOME/.asdf/asdf.sh" ]]; then
     source $HOME/.asdf/asdf.sh
   fi
 
+
+  [[ -d $HOME/.asdf/plugins/golang ]] && source $HOME/.asdf/plugins/golang/set-env.zsh
+
   fpath=(${ASDF_DIR}/completions $fpath)
 fi
 
