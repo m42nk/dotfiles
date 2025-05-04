f070369ba56a8e9649be44d1a3a3db86e5d76fc5
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2025-05-04 15:50:26 +0700

    update

--- packages/sketchybar/plugins/bluetooth.sh
+++ packages/sketchybar/plugins/bluetooth.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# Define colors
+COLOR_BLUE=0xff89b4fa # you can change this to your preferred blue
+COLOR_WHITE=0xffffffff
+
 # Fetch the name of the currently connected Bluetooth device
 bluetooth_device=$(system_profiler -json SPBluetoothDataType | jq -r '.SPBluetoothDataType[0].device_connected[0] | keys | .[0]')
 
 if [ -z "$bluetooth_device" ]; then
   ICON=󰂲
-  sketchybar --set "$NAME" icon="$ICON" label=""
+  sketchybar --set "$NAME" icon="$ICON" label="" icon.color=$COLOR_WHITE
   exit 0
 fi
 
 ICON=󰂯
-sketchybar --set "$NAME" icon="$ICON" label="$bluetooth_device"
+sketchybar --set "$NAME" icon="$ICON" label="$bluetooth_device" icon.color=$COLOR_BLUE label.color=$COLOR_BLUE
